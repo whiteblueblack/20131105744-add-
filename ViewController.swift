a13f3c559042d1b4f7cae99cb5225c59f272e1ca
whiteblueblack <zh13@Mac-29.local>
2016-03-17 11:58:24 +0800

    add
    add

---
+++ ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  ADD
+//
+//  Created by ZH13 on 16/3/17.
+//  Copyright © 2016年 ZH13. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var x: UITextField!
+    @IBOutlet weak var y: UITextField!
+    @IBOutlet weak var z: UITextField!
+    @IBAction func add(sender: UIButton){
+    var a:Double=0
+    var b:Double=0
+    var c:Double=0
+    if(!x.text!.isEmpty) {
+        a=(x.text! as NSString).doubleValue
+    }
+    if(!y.text!.isEmpty){
+        b=(y.text! as NSString).doubleValue
+    }
+        c=a+b
+        z.text="\(c)"
+        
+}
+    override func viewDidLoad(){
+        super.viewDidLoad()
+}
+
+    override func didReceiveMemoryWarning(){
+        super.didReceiveMemoryWarning()
+}
+}
+    
+
+
+
