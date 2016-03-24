0e824654308f0e9b2babbecb11d675a29edc51b4
whiteblueblack <zh13@Mac-29.local>
2016-03-24 11:56:10 +0800

    add进度不怎么样
    要加快进度

--- ViewController.swift
+++ ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var x: UITextField!
     @IBOutlet weak var y: UITextField!
     @IBOutlet weak var z: UITextField!
+    
     @IBAction func add(sender: UIButton){
     var a:Double=0
     var b:Double=0
